0fae643711714e766f89bf2df481e1068a92239d
yamatyuu-ibuki <yamatyuu1121@gmail.com>
2020-11-25 02:24:57 +0900

    login-mypage

---
+++ 4each_login_mypage/register_insert.php
@@ -0,0 +1,20 @@
+﻿<?php
+mb_internal_encoding("utf8");
+
+// 自分のMySQLの設定により異なります。
+$pdo = new PDO("mysql:dbname=lesson01;host=localhost;","root","");
+
+$stmt = $pdo->prepare("insert into login_mypage (name,mail,password,picture,comments)values(?,?,?,?,?)");
+
+$stmt->bindValue(1,$_POST['name']);
+$stmt->bindValue(2,$_POST['mail']);
+$stmt->bindValue(3,$_POST['password']);
+$stmt->bindValue(4,$_POST['path_filename']);
+$stmt->bindValue(5,$_POST['comments']); 
+
+$stmt->execute();
+
+$pdo = NULL;
+
+header('Location:after_register.html');
+?>
